16d89727a3221f4386e5218093315ac82ab3c7e5
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-03-24 16:13:29 +1300

    local systems background research
    Signed-off-by: Jason Cairns <jcai849@aucklanduni.ac.nz>

--- bib/cites.R
+++ bib/cites.R
@@ -1,15 +1,25 @@
 options(citation.bibtex.max=999)
 
-noncran <- c("distributedR", "RHadoop", "RHIPE")
+noncran <- c("distributedR", "RHadoop", "RHIPE", "hmr", "bigalgebra",
+             "ffbase2")
 cran <- c("base", "foreach", "doParallel", "doSNOW", "doMPI",
           "future", "furrr", "partools", "pbdBASE", "Rmpi",
           "sparklyr", "SparkR", "doFuture", "future.batchtools",
-          "future.apply", "future.callr", "batchtools", "callr")
+          "future.apply", "future.callr", "batchtools", "callr",
+          "snow", "bigmemory", "biganalytics", "bigtabulate",
+          "biglasso", "bigstatsr", "disk.frame", "data.table", "fst",
+          "iotools", "ff", "biglm", "ffbase")
 
+install.packages(x)
 ## install.packages(cran)
+## install.packages("hmr",,"http://rforge.net")
+## install.packages(c("RSclient","Rserve"), repos = "https://rforge.net")
+## install.packages("big.data.table", repos = "http://jangorecki.gitlab.io/big.data.table")
+## devtools::install_github("edwindj/ffbase2")
 
 sink("r-packages.txt")
-sapply(cran, citation)
+sapply(c(cran, noncran),
+       function(x)toBibtex(citation(x)))
 sink()
 
 ## make vectorised version of citation outputting only bibtex
