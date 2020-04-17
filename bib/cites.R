f5ffbe85954d7b32c397409a86b22d01f08442a9
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-04-17 17:18:25 +1200

    detail on pbdR init

--- bib/cites.R
+++ bib/cites.R
@@ -2,7 +2,7 @@ options(citation.bibtex.max=999)
 
 noncran <- c("distributedR", "RHadoop", "RHIPE", "hmr", "bigalgebra",
              "ffbase2", "pbdDMAT", "pbdML", "hpcvis", "pbdCS",
-             "doAzureParallel")
+             "doAzureParallel", "pbdSLAP", "RBigData/pmclust")
 cran <- c("base", "foreach", "doParallel", "doSNOW", "doMPI",
           "future", "furrr", "partools", "pbdBASE", "Rmpi",
           "sparklyr", "SparkR", "doFuture", "future.batchtools",
@@ -10,10 +10,11 @@ cran <- c("base", "foreach", "doParallel", "doSNOW", "doMPI",
           "snow", "bigmemory", "biganalytics", "bigtabulate",
           "biglasso", "bigstatsr", "disk.frame", "data.table", "fst",
           "iotools", "ff", "biglm", "ffbase", "pbdMPI", "doRNG",
-          "doRedis", "promises", "graphframes")
+          "doRedis", "promises", "graphframes", "rbenchmark", "microbenchmark",
+	  "kazaam")
 
-
-## remotes::install_github(c("wrathematics/getip", "RBigData/hpcvis", "RBigData/pbdCS", "RBigData/pbdDMAT", "RBigData/pbdML", "edwindj/ffbase2", "Azure/rAzureBatch", "Azure/doAzureParallel"))
+## install.packages(cran)
+## remotes::install_github(c("wrathematics/getip", "RBigData/hpcvis", "RBigData/pbdCS", "RBigData/pbdDMAT", "RBigData/pbdML", "edwindj/ffbase2", "Azure/rAzureBatch", "Azure/doAzureParallel", "RBigData/pmclust", "RBigData/pbdSLAP"))
 ## install.packages(c("RSclient","Rserve", "hmr"), repos = "https://rforge.net")
 ## install.packages("big.data.table", repos = "http://jangorecki.gitlab.io/big.data.table")
 
