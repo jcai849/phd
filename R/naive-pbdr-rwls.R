41446832e937793e0eed56a5cc8308cb1d248405
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-04-27 17:40:17 +1200

    first complete pbdDMAT RWLS implementation and writeup

--- R/naive-pbdr-rwls.R
+++ R/naive-pbdr-rwls.R
@@ -1,13 +1,16 @@
+suppressMessages(library(pbdDMAT))
+init.grid()
+
 set.seed(1234)
 # Generate the data
 
 n <- 1000
-B <- matrix(c(1,3))
+B <- ddmatrix(c(1,3))
 x0 <- rep(1, n)
 x1 <- rnorm(n, 0, 1)
-X <- cbind(x0, x1)
+X <- as.ddmatrix(cbind(x0, x1))
 p <- 1 / (1 + exp(- X %*% B))
-y <- rbinom(n, 1, p)
+y <- ddmatrix(rbinom(n, 1, as.vector(p)))
 
 # Base comparison
 #glm(y ~ x1, family = "binomial")
@@ -20,14 +23,15 @@ logReg <- function(X, y, maxIter=80, tolerance=0.01){
 	}
 	##
 	weights <- function(X, B, y){
-		diag(as.vector(pr(X, B)))
+		diag(as.vector(pr(X, B)), type="ddmatrix")
 	}
 	##
-	oldB <- matrix(c(Inf,Inf))
-	newB <- matrix(c(0, 0))
-	nIter <- 0
-	while (colSums((newB - oldB)^2) > tolerance &&
-	       nIter < maxIter) {
+	oldB <- ddmatrix(c(Inf,Inf))
+	newB <- ddmatrix(c(0, 0))
+	nIter <- ddmatrix(0)
+	maxIter <- as.ddmatrix(maxIter)
+	while (as.matrix(colSums((newB - oldB)^2) > tolerance &
+	       nIter < maxIter)) {
 		oldB <- newB
 	## N-R as RWLS
 		W <- weights(X, oldB, y)
@@ -41,3 +45,5 @@ logReg <- function(X, y, maxIter=80, tolerance=0.01){
 }
 
 print(logReg(X, y, tolerance=1E-6, maxIter=100))
+
+finalize()
