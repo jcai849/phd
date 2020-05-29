65a60246efc4c1ec00b0b8576f3cc7c205862bcd
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-05-29 17:49:38 +1200

    distribute objects

--- R/make-RS-cluster.R
+++ R/make-RS-cluster.R
@@ -1,3 +1,8 @@
+library(RSclient)
+library(uuid)
+
+hosts <- paste0("hadoop", 1:8)
+
 do_servers <- function(command) 
 	function(hosts) 
 		lapply(hosts, function(host)
@@ -12,5 +17,38 @@ connect_servers <- function(hosts) sapply(hosts,
 					  USE.NAMES = TRUE)
 
 make_cluster <- function(hosts){
-	servers <- start_servers(hosts)
+	try(start_servers(hosts))
+	servers <- connect_servers(hosts)
+	lapply(servers, function(node) {class(node) <- c("node", class(node))})
+	class(servers) <- c("cluster", class(servers))
+	servers
+}
 
+send <- function(to, obj, id = UUIDgenerate()){
+	UseMethod("send", to)
+}
+
+send.node <- function(to, obj, id = UUIDgenerate()){
+	RS.assign(to, id, obj)
+	dist_ref <- list(host = to, name = id, from = 0, to = length(obj))
+	class(dist_ref) <- "distributed.object"
+	dist_ref
+}
+
+send.cluster <- function(to, obj, id = UUIDgenerate()){
+	clustsize <- length(to)
+	objsize <- length(obj)
+	objelems <- seq(length(obj))
+	splits <- split(obj, cumsum(floor(objelems %% (objsize / clustsize)) == 1))
+	names(splits) <- names(to)
+	lapply(seq(clustsize), function(i) send(to[[i]], splits[[i]], id))
+	dist_ref <- list(host = to, name = id, from = sapply(splits, head, 1), to = sapply(splits, tail, 1))
+	class(dist_ref) <- "distributed.object"
+	dist_ref
+}
+
+rsc <- make_cluster(hosts)
+send(rsc[[1]], 1:100)
+RS.eval(rsc[[2]], quote({ls()}))
+RS.eval(rsc[[2]], quote({get(ls())}))
+x = send(rsc, 1:100)
