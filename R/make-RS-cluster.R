d1c75470aca9a4d6ca254dd13952d7832c3acfc2
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-06-01 13:51:35 +1200

    Ops defined under strict conditions

--- R/make-RS-cluster.R
+++ R/make-RS-cluster.R
@@ -1,8 +1,6 @@
 library(RSclient)
 library(uuid)
 
-hosts <- paste0("hadoop", 1:8)
-
 do_servers <- function(command) 
 	function(hosts) 
 		lapply(hosts, function(host)
@@ -10,7 +8,7 @@ do_servers <- function(command)
 					      wait = FALSE))
 
 start_servers <- do_servers("R CMD Rserve --vanilla")
-kill_servers <- do_servers("killall Rserve")
+kill_servers <- do_servers("killall Rserve") # Find a cleaner way to do this!!
 connect_servers <- function(hosts) sapply(hosts, 
 					  function(host) RS.connect(host),
 					  simplify = FALSE,
@@ -24,31 +22,71 @@ make_cluster <- function(hosts){
 	servers
 }
 
-send <- function(to, obj, id = UUIDgenerate()){
+send <- function(to, obj){
+	id <- UUIDgenerate()
 	UseMethod("send", to)
 }
 
-send.node <- function(to, obj, id = UUIDgenerate()){
+send.node <- function(to, obj){
 	RS.assign(to, id, obj)
 	dist_ref <- list(host = to, name = id, from = 0, to = length(obj))
-	class(dist_ref) <- "distributed.object"
+	if (is.vector(obj)) {
+		class(dist_ref) <- c("distributed.vector", class(dist_ref))
+	}
+	class(dist_ref) <- c("distributed.object", class(dist_ref))
 	dist_ref
 }
 
-send.cluster <- function(to, obj, id = UUIDgenerate()){
+send.cluster <- function(to, obj){
 	clustsize <- length(to)
 	objsize <- length(obj)
 	objelems <- seq(length(obj))
-	splits <- split(obj, cumsum(floor(objelems %% (objsize / clustsize)) == 1))
+	spliton <- floor(objelems %% (objsize / clustsize)) == 1
+	splits <- split(obj,
+			cumsum(spliton))
 	names(splits) <- names(to)
-	lapply(seq(clustsize), function(i) send(to[[i]], splits[[i]], id))
-	dist_ref <- list(host = to, name = id, from = sapply(splits, head, 1), to = sapply(splits, tail, 1))
-	class(dist_ref) <- "distributed.object"
+	lapply(seq(clustsize), function(i) RS.assign(to[[i]], id, splits[[i]]))
+	dist_ref <- list(host = to, name = id,
+			 from = which(spliton), 
+			 to = c(which(spliton)[-1] - 1, objsize))
+	if (is.vector(obj)) {
+		class(dist_ref) <- c("distributed.vector", class(dist_ref))
+	}
+	class(dist_ref) <- c("distributed.object", class(dist_ref))
 	dist_ref
 }
 
+Ops.distributed.vector <- function(e1, e2) {
+	id <- UUIDgenerate()
+	lapply(e1$host, function(hostname) eval(
+	    bquote(RS.eval(.(hostname),  # is this R or lisp??
+			    quote(assign(.(id), 
+					 do.call(.(.Generic), 
+					  list(get(.(e1$name)),
+					       get(.(e2$name))))))))
+	))
+	e1$name <- id
+	e1
+}
+
+##################################  DEMO  ######################################
+
+hosts <- paste0("hadoop", 1:8)
 rsc <- make_cluster(hosts)
-send(rsc[[1]], 1:100)
-RS.eval(rsc[[2]], quote({ls()}))
-RS.eval(rsc[[2]], quote({get(ls())}))
 x = send(rsc, 1:100)
+RS.eval(rsc[[2]], quote(ls()))
+RS.eval(rsc[[2]], quote({get(ls()[length(ls())])})) # is this R or APL?
+
+a = send(rsc, 100:0)
+b = send(rsc, 400:300)
+c = a + b
+a.on.2 = eval(bquote(RS.eval(rsc[[2]], quote(get(.(a$name))))))
+b.on.2 = eval(bquote(RS.eval(rsc[[2]], quote(get(.(b$name))))))
+c.on.2 = eval(bquote(RS.eval(rsc[[2]], quote(get(.(c$name))))))
+
+a.on.2 + b.on.2
+c.on.2
+
+all.equal(a.on.2 + b.on.2, c.on.2)
+
+kill_servers(hosts)
