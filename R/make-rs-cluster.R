f77e6e72e947ddb1d35a050903dbefd3aaa06549
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-06-05 16:58:20 +1200

    lay foundations for inter-node comms

--- R/make-rs-cluster.R
+++ R/make-rs-cluster.R
@@ -9,51 +9,77 @@ do_servers <- function(command)
 
 start_servers <- do_servers("R CMD Rserve --vanilla")
 kill_servers <- do_servers("killall Rserve") # Find a cleaner way to do this!!
-connect_servers <- function(hosts) sapply(hosts, 
-					  function(host) RS.connect(host),
-					  simplify = FALSE,
-					  USE.NAMES = TRUE)
+connect_servers <- function(hosts) {
+	sapply(hosts, 
+	       function(host) {
+		       RS.connect(host)
+	       }, simplify = FALSE, USE.NAMES = TRUE)
+}
 
 make_cluster <- function(hosts){
 	try(start_servers(hosts))
+	Sys.sleep(2)
 	servers <- connect_servers(hosts)
+	lapply(servers, function(host) {
+		       lapply(c("RSclient", "uuid"),
+			      function(lib) eval(bquote(RS.eval(host, library(.(lib))))))
+		       lapply(c("receive", "receive.distributed.object", "connect_servers"),
+			      function(fun) eval(bquote(RS.assign(host, fun, .(get(fun))))))
+	       })
 	lapply(servers, function(node) {class(node) <- c("node", class(node))})
 	class(servers) <- c("cluster", class(servers))
 	servers
 }
 
-send <- function(to, obj){
+`[.cluster` <- function(x, i){
+		classes <- class(x)
+		class(x) <- NULL
+		out <- x[i]
+		class(out) <- classes
+		out
+}
+
+send <- function(obj, to){
 	id <- UUIDgenerate()
 	UseMethod("send", to)
 }
 
-send.node <- function(to, obj){
-	RS.assign(to, id, obj)
-	dist_ref <- list(host = to, name = id, from = 0, to = length(obj))
-	if (is.vector(obj)) {
-		class(dist_ref) <- c("distributed.vector", class(dist_ref))
-	}
-	class(dist_ref) <- c("distributed.object", class(dist_ref))
-	dist_ref
+send.node <- function(obj, to){
+		RS.assign(to, id, obj)
+		dist_ref <- list(host = to, name = id, from = 0, to = length(obj))
+		if (is.vector(obj)) {
+			class(dist_ref) <- c("distributed.vector", class(dist_ref))
+		}
+		class(dist_ref) <- c("distributed.object", class(dist_ref))
+		dist_ref
 }
 
-send.cluster <- function(to, obj){
-	clustsize <- length(to)
-	objsize <- length(obj)
-	objelems <- seq(length(obj))
-	spliton <- floor(objelems %% (objsize / clustsize)) == 1
-	splits <- split(obj,
-			cumsum(spliton))
-	names(splits) <- names(to)
-	lapply(seq(clustsize), function(i) RS.assign(to[[i]], id, splits[[i]]))
-	dist_ref <- list(host = to, name = id,
-			 from = which(spliton), 
-			 to = c(which(spliton)[-1] - 1, objsize))
-	if (is.vector(obj)) {
-		class(dist_ref) <- c("distributed.vector", class(dist_ref))
-	}
-	class(dist_ref) <- c("distributed.object", class(dist_ref))
-	dist_ref
+send.cluster <- function(obj, to){
+		clustsize <- length(to)
+		objsize <- length(obj)
+		objelems <- seq(length(obj))
+		spliton <- if (clustsize < objsize) {
+			bucketsto <- cumsum(rep(objsize / clustsize, clustsize))
+			bucketsfrom <- c(0, bucketsto[-clustsize])
+			facsplit = sapply(objelems,
+			       function(i) which(i > bucketsfrom &
+						 i <= bucketsto))
+			c(TRUE, !{c(NA, facsplit[-objsize]) - facsplit == 0}[-1])
+		} else rep(TRUE, objsize)
+		splits <- split(obj,
+				cumsum(spliton))
+		allsplitrefs <- seq(length(splits))
+		names(splits) <- names(to)[allsplitrefs]
+		lapply(seq(length(splits)),
+		       function(i) RS.assign(to[[i]], id, splits[[i]]))
+		dist_ref <- list(host = to[allsplitrefs], name = id,
+				 from = which(spliton), 
+				 to = c(which(spliton)[-1] - 1, objsize))
+		if (is.vector(obj)) {
+			class(dist_ref) <- c("distributed.vector", class(dist_ref))
+		}
+		class(dist_ref) <- c("distributed.object", class(dist_ref))
+		dist_ref
 }
 
 Ops.distributed.vector <- function(e1, e2) {
@@ -66,37 +92,47 @@ Ops.distributed.vector <- function(e1, e2) {
 		    all.equal(e1$to, e2$to) &
 		    all.equal(e1$from, e2$from)) {
 			lapply(e1$host, function(hostname) eval(
-			    bquote(RS.eval(.(hostname),
-				    quote(assign(.(id), 
-						 do.call(.(.Generic), 
-						  list(get(.(e1$name)),
-						       get(.(e2$name))))))))))
+			    bquote(RS.eval(hostname,
+			     quote(assign(.(id), 
+				 do.call(.(.Generic), 
+				  list(get(.(e1$name)),
+				       get(.(e2$name))))))))))
 			e1$name <- id
 			return(e1)
-		} else {
-			do.call(.Generic, match_host_indices(e1, e2))
+		} else if (length(e1) == length(e2) |
+			   length(e1) > length(e2)) {
+			do.call(.Generic, list(e1, align(e2, e1)))
+		} else if (length(e1) < length(e2)) {
+			do.call(.Generic, list(align(e1, e2), e2))
 		}
 	} else if (!("distributed.vector" %in% e1.classes)) {
-		e1 <- send(e2$host, e1)
+		e1 <- send(e1, e2$host)
 		do.call(.Generic, list(e1, e2))
 	} else if (!("distributed.vector" %in% e2.classes)) {
-		e2 <- send(e1$host, e2)
+		e2 <- send(e2, e1$host)
 		do.call(.Generic, list(e1, e2))
 	}
 }
 
-# returns a list of the two objects now with matching elements on matching hosts
-match_host_indices <- function(e1, e2){
-	
+align <- function(from, to){
+	stop("TODO: Implement align")
 }
 
-receive <- function(obj) {
+receive <- function(obj, remote=FALSE) {
 	UseMethod("receive", obj)
 }
 
-receive.distributed.object <- function(obj) {
-	recv <- lapply(obj$host, function(hostname) eval(bquote(RS.eval(.(hostname),
-							   quote(get(.(obj$name)))))))
+receive.distributed.object <- function(obj, remote=FALSE) {
+	if (remote) {
+		conn <- connect_servers(names(obj$host))
+		on.exit(lapply(conn, RS.close))
+	} else {
+		conn <- obj$host
+	}
+	recv <- lapply(conn, function(hostname) {
+			       eval(bquote(RS.eval(hostname,
+						   quote(get(.(obj$name))))))
+			   })
 	unsplit(recv, rep(seq(length(recv)), 1 + obj$to - obj$from))
 }
 
@@ -107,13 +143,13 @@ peek <- function(loc) {
 peek.cluster <- function(loc) {
 	hosts <- names(loc)
 	sapply(hosts, function(hostname) {
-		       sapply(RS.eval(rsc[[hostname]], quote(ls())),
-						function(uuid) {
-							eval(bquote(RS.eval(.(rsc[[hostname]]), 
-									    quote(get(.(uuid))))))
-						},
-						simplify = FALSE, USE.NAMES = TRUE),
-			 }
+	       sapply(RS.eval(rsc[[hostname]], quote(ls())),
+			function(uuid) {
+				eval(bquote(RS.eval(.(rsc[[hostname]]), 
+						    quote(get(.(uuid))))))
+			},
+			simplify = FALSE, USE.NAMES = TRUE)
+		 },
 	simplify = FALSE, USE.NAMES = TRUE)
 }
 
@@ -130,13 +166,13 @@ length.distributed.vector <- function(x) max(x$to)
 hosts <- paste0("hadoop", 1:8)
 
 rsc <- make_cluster(hosts)
-x = send(rsc, 1:100)
+x = send(1:100, rsc)
 
-peek(rsc[[hosts[1]]])
+peek(rsc[[1]])
 peek(rsc)
 
-a = send(rsc, 0:100)
-b = send(rsc, 400:300)
+a = send(0:100, rsc)
+b = send(400:300, rsc)
 c = a - b
 c.local = receive(a) - receive(b)
 all.equal(receive(c), c.local)
@@ -149,5 +185,10 @@ b. = 400:300
 c.. = a - b.
 all.equal(receive(c..), c.local)
 
+y = send(seq(10), rsc)
+z = send(3, rsc)
+
+id <- getUUID()
+k = eval(bquote(RS.eval(rsc[[1]], receive(.(a), remote = TRUE))))
 
 kill_servers(hosts)
