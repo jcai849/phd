4b22473426905dbbb697ee02faced473f73eca75
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-06-21 17:51:31 +1200

    add distributed decision tree tests

--- R/experiment-dist-decision-tree-test.R
+++ R/experiment-dist-decision-tree-test.R
@@ -4,3 +4,15 @@ X <- mtcars[,c("vs", "am", "gear", "carb")]
 y <- mtcars$cyl
 tree <- dist_decision_tree(X, y)
 predict(tree, X)
+
+# Distributed Decision Tree
+
+hosts <- paste0("hadoop", 1:8)
+rsc <- make_cluster(hosts)
+
+dX <- as.distributed(mtcars[,c("vs", "am", "gear", "carb")], rsc)
+dy <- as.distributed(mtcars$cyl, rsc)
+dtree <- dist_decision_tree(dX, dy)
+predict(dtree, X)
+
+kill_servers(hosts)
