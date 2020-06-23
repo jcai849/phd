fb4627084c23f4e59c9bbfb48a56a1bf62bf0ebf
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-06-23 12:23:08 +1200

    added big flights to experiment on; working

--- R/experiment-dist-decision-tree-test.R
+++ R/experiment-dist-decision-tree-test.R
@@ -15,4 +15,27 @@ dy <- as.distributed(mtcars$cyl, rsc)
 dtree <- dist_decision_tree(dX, dy)
 predict(dtree, X)
 
+# Big Distributed Decision Tree
+
+cols = c("Year"="integer","Month"="integer","DayofMonth"="integer",
+	 "DayOfWeek"="integer","DepTime"="integer","CRSDepTime"="integer",
+	 "ArrTime"="integer","CRSArrTime"="integer",
+	 "UniqueCarrier"="character","FlightNum"="integer","TailNum"="character",
+	 "ActualElapsedTime"="integer","CRSElapsedTime"="integer",
+	 "AirTime"="integer","ArrDelay"="integer", "DepDelay"="integer",
+	 "Origin"="character","Dest"="character","Distance"="integer",
+	 "TaxiIn"="integer","TaxiOut"="integer", "Cancelled"="integer",
+	 "CancellationCode"="character","Diverted"="integer",
+	 "CarrierDelay"="integer","WeatherDelay"="integer","NASDelay"="integer",
+	 "SecurityDelay"="integer","LateAircraftDelay"="integer")
+big <- read.distributed.csv(file = "~/flights-chunk.csv",
+			    header = FALSE,
+			   col.names = names(cols),
+			   colClasses = as.vector(cols),
+			    to = rsc)
+nrow(big)
+bigX <- big[,c("Month", "DayOfWeek")]
+bigy <- big[,"Diverted"]
+bigtree <- dist_decision_tree(bigX, bigy)
+
 kill_servers(hosts)
