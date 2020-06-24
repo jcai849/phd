fe919733cfe96df3b9081c7f6d661e83860daaa5
Jason Cairns <jcai849@aucklanduni.ac.nz>
2020-06-24 16:47:56 +1200

    add logical indexing tests

--- R/experiment-eager-dist-obj-test.R
+++ R/experiment-eager-dist-obj-test.R
@@ -9,8 +9,8 @@ RS.eval(rsc[[1]], ls())
 (v1 <- as.distributed(1:150, rsc))
 (v2 <- as.distributed(151:300, rsc))
 (v3 <- as.distributed(1:150 %% 2 == 0, rsc))
-(v4 <- send(1, rsc, align_to=v1))
-(v5 <- send(1, rsc, align_to=v3))
+(v4 <- as.distributed(1, rsc, align_to=v1))
+(v5 <- as.distributed(1, rsc, align_to=v3))
 (v6 <- as.distributed(structure(1:26, names = letters), rsc))
 v6[]
 RS.eval(rsc[[1]], ls())
@@ -30,7 +30,7 @@ RS.eval(rsc[[1]], ls())
 (i1 <- v1[1])
 (i2 <- v1[30:50])
 (i3 <- v1[v3])
-##i4 <- v1[1:150 %% 2 == 0]
+(i4 <- v1[1:150 %% 2 == 0])
 
 # Distributed Data Frame Coercion
 
@@ -42,24 +42,24 @@ d1[,]
 d1[,3]
 d1[,3:5]
 d1[,"Sepal.Length"]
-#d1[,as.distributed.vector("Sepal.Length", rsc)]
-#d1[,as.distributed.vector(c(T,F,T,F,F), rsc)]
+#d1[,as.distributed("Sepal.Length", rsc)]
+#d1[,as.distributed(c(T,F,T,F,F), rsc)]
 
 d1[3, ]
 d1[3, 3]
 d1[3, 3:5]
 d1[3, "Sepal.Length"]
-#d1[3, as.distributed.vector("Sepal.Length", rsc)]
-#d1[3, as.distributed.vector(c(T,F,T,F,F), rsc)]
+#d1[3, as.distributed("Sepal.Length", rsc)]
+#d1[3, as.distributed(c(T,F,T,F,F), rsc)]
 
 d1[3:80, ]
 d1[3:80, 3]
 d1[3:80, 3:5]
 d1[3:80, "Sepal.Length"]
-#d1[3:80, as.distributed.vector("Sepal.Length", rsc)]
-#d1[3:80, as.distributed.vector(c(T,F,T,F,F), rsc)]
+#d1[3:80, as.distributed("Sepal.Length", rsc)]
+#d1[3:80, as.distributed(c(T,F,T,F,F), rsc)]
 
-#d1[v1, ]
+#d1[v1, ] # it works but it shouldn't!!
 #d1[v1, 3]
 #d1[v1, 3:5]
 #d1[v1, "Sepal.Length"]
@@ -73,10 +73,10 @@ d1[v3, "Sepal.Length"]
 #d1[v3, as.distributed.vector("Sepal.Length", rsc)]
 #d1[v3, as.distributed.vector(c(T,F,T,F,F), rsc)]
 
-## d1[1:150 %% 2 == 0, ]
-## d1[1:150 %% 2 == 0, 3]
-## d1[1:150 %% 2 == 0, 3:5]
-## d1[1:150 %% 2 == 0, "Sepal.Length"]
+d1[1:150 %% 2 == 0, ]
+d1[1:150 %% 2 == 0, 3]
+d1[1:150 %% 2 == 0, 3:5]
+d1[1:150 %% 2 == 0, "Sepal.Length"]
 ## d1[1:150 %% 2 == 0, as.distributed.vector("Sepal.Length", rsc)]
 ## d1[1:150 %% 2 == 0, as.distributed.vector(c(T,F,T,F,F), rsc)]
 
@@ -100,11 +100,12 @@ lapply(d1, receive)
 unique(d1$Species)
 table(d1$Species)
 d1$Species %in% c("virginica", "setosa")
+head(d1[d1$Species %in% c("virginica", "setosa"),][])
 
 # gc
 
 RS.eval(rsc[[1]], ls())
-rm(list = c(paste0("v", 1:6), paste0("o", 1:5), paste0("i", 1:3), "d1"))
+rm(list = c(paste0("v", 1:6), paste0("o", 1:5), paste0("i", 1:4), "d1"))
 gc()
 RS.eval(rsc[[1]], ls())
 
