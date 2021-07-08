5a66609a1b9862cb894646e52ac2149363de6c2e
Jason Cairns <jcai849@aucklanduni.ac.nz>
2021-07-08 15:10:05 +1200

    mirror dlm from lsmr

--- src/dlm.R
+++ src/dlm.R
@@ -1,18 +1,23 @@
-dlm <-
-function(formula, data, weights=NULL, sandwich=FALSE) {
-        stopifnot(largeScaleR::is.distObjRef(data))
-        chunks <- largeScaleR::chunkRef(data)
-        stopifnot(length(chunks) > 0L)
-	sys.call <- largeScaleR::currCallFun()
-        init <- largeScaleR::do.ccall("biglm::biglm",
-                         list(formula=largeScaleR::envBase(formula),
-                              data=chunks[[1]],
-                              weights=largeScaleR::envBase(weights),
-                              sandwich=sandwich),
-                         target=chunks[[1]],
-                         insert=list(sys.call=largeScaleR::envBase(sys.call)))
-        if (length(chunks) != 1L)
-		largeScaleR::dreduce("biglm::update.biglm",
-				     largeScaleR::distObjRef(chunks[-1]), init)
-        else init
+dlm <- function(formula, data, weights=NULL, sandwich=FALSE) {
+	stopifnot(largeScaleR::is.distObjRef(data))
+	chunks <- largeScaleR::chunkRef(data)
+	stopifnot(length(chunks) > 0L)
+	dblm <- dbiglm(formula, chunks[[1]], weights, sandwich)
+	if (length(chunks) != 1L)
+		largeScaleR::dreduce(f="biglm::update.biglm",
+				     x=largeScaleR::distObjRef(chunks[-1]),
+				     init=dblm)
+	else init
+}
+
+dbiglm <- function(formula, data, weights=NULL, sandwich=FALSE) {
+	stopifnot(largeScaleR::is.chunkRef(data))
+	sys.call <- largeScaleR::currCallFun(-1)
+	largeScaleR::do.ccall(what="biglm::biglm",
+			      args=list(formula=largeScaleR::envBase(formula),
+					data=data,
+					weights=largeScaleR::envBase(weights),
+					sandwich=sandwich),
+			      target=data,
+			      insert=list(sys.call=largeScaleR::envBase(sys.call)))
 }
