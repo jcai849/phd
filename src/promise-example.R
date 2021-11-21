bb04c4dd5bd72aae4a62dd4154b5e2db2a2f2531
Jason Cairns <jason.cairns@aucklanduni.ac.nz>
2021-11-22 08:45:33 +1300

    give example of central process with promises

---
+++ src/promise-example.R
@@ -0,0 +1,10 @@
+plist <- lapply(prerequisites(request),
+       function(chunk)
+           promise(function(resolve, reject)
+                       resolve(emerge(chunk))))
+pall <- promise_all(p)
+worked <- then(pall,
+               onFulfilled=function(value)
+                   promise(function(resolve, reject)
+                               tryCatch(resolve(do.call(computation(request), value)),
+                                        error=reject))
