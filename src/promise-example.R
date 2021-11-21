f2a2216b1404a46acfdf023d5bfaf5410a717220
Jason Cairns <jason.cairns@aucklanduni.ac.nz>
2021-11-22 10:30:34 +1300

    mcparallel instead of mclapply

--- src/promise-example.R
+++ src/promise-example.R
@@ -14,13 +14,13 @@ worked <- then(pall,
 # Explicit Asynchrony
 plist <- lapply(prerequisites(request),
        function(chunk)
-           promise(function(resolve, reject) mclapply(
+           promise(function(resolve, reject) mcparallel(
                            resolve(emerge(chunk)),
                        detached=TRUE)))
 pall <- promise_all(p)
 worked <- then(pall,
                onFulfilled=function(value)
-                   promise(function(resolve, reject) mclapply(
+                   promise(function(resolve, reject) mcparallel(
                                    tryCatch(resolve(do.call(computation(request),value)),
                                             error=reject)),
                                detached=TRUE))
